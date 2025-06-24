f781de96e21688c9d1da50a52f45297dec921af9
danesh0611 <messidhanesh2006@gmail.com>
2025-06-24 22:10:37 +0530

    second

--- imdb_clone/src/components/Movies.jsx
+++ imdb_clone/src/components/Movies.jsx
@@ -1,39 +1,51 @@
-import React from 'react'
-import Moviecard from './Moviecard'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import Pagination from './Pagination'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Moviecard from './Moviecard';
+import Pagination from './Pagination';
 
 function Movies() {
-    const [movies, setMovies] = useState([])
-    useEffect(()=>{
-        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=59af75de003937def5f683f24889a4f2&language=en-US&page=1`).then((res)=>{
-            setMovies(res.data.results)
-        },[])
-    })
-    return (
-        <>
-       
-            <div className='text-2xl text-green-700 font-bold p-3 text-center margin-top-3'>
-                Trending Movies
-            </div>
-            <div className='flex flex-wrap justify-around'>
-                {movies.map((movie)=>{
-                    return(
-                         <Moviecard poster_path={movie.poster_path} name={movie.original_title}/>
-                    )
-                })}
+  const [movies, setMovies] = useState([]);
 
-              
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const apiKey = '59af75de003937def5f683f24889a4f2';
+      const baseUrl = 'https://api.themoviedb.org/3/discover/movie';
+      const languages = ['en', 'ta', 'hi', 'te'];
 
-             
-                </div>
-                <Pagination/>
-                
-                </>
+      try {
+        const responses = await Promise.all(
+          languages.map(lang =>
+            axios.get(`${baseUrl}?api_key=${apiKey}&with_original_language=${lang}`)
+          )
+        );
 
-    )
+        const allMovies = responses.flatMap(res => res.data.results);
+        const uniqueMovies = Array.from(new Map(allMovies.map(m => [m.id, m])).values());
+
+        setMovies(uniqueMovies);
+      } catch (err) {
+        console.error('Error fetching movies:', err);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <>
+      <div className='text-2xl text-green-700 font-bold p-3 text-center mt-3'>
+        Trending movies
+      </div>
+
+      <div className='flex flex-wrap justify-around'>
+        {movies.map((movie) => (
+          <Moviecard key={movie.id} poster_path={movie.poster_path} name={movie.original_title} />
+        ))}
+      </div>
+
+      <Pagination />
+    </>
+  );
 }
 
-export default Movies
-//https://api.themoviedb.org/3/movie/popular?api_key=59af75de003937def5f683f24889a4f2&language=en-US&page=1
+export default Movies;
